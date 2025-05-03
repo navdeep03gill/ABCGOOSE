a93a263670dd7b63cd2e093c95ca7122c64ddf32
navdeep03gill <navdeep03gill@gmail.com>
2025-05-02 21:20:20 -0400

    refactored app.py for render

--- database/app.py
+++ database/app.py
@@ -21,6 +21,10 @@ def create_app():
     
     return app
 
-if __name__ == '__main__': 
-    app = create_app()
-    app.run(host='0.0.0.0', port=8000) #app.run(host='0.0.0.0', port=8000)
+app = create_app()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('SUPABASE_PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=8000) #app.run(host='0.0.0.0', port=8000)
+
